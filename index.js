be1f733ab8285aea849c731d8f4e1246c2205ce2
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-27 15:29:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,14 @@ function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
 }
 
 function deleteFromDriverByKey (driver, key) {
-  
+  function deleteFromDriverByKey (driver, key) {
+  const newObj = Object.assign({}, driver);
+  delete newObj[key];
+  return newObj;
+}
+
+ function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver [key];
+  return driver;
+
 }
